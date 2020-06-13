1122518a1ca969a51be2e52e8ddbafa3c20b56d1
Lewis Porter <lewis.porter@vualto.com>
2020-06-13 12:20:02 +0100

    new file:   .gitignore 	new file:   fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,20 @@
+"""
+    Write a Python program which iterates the integers from 1 to 50. For multiples of three print "Fizz" instead of
+    the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print
+    "FizzBuzz".
+"""
+
+
+# Function to check whether the given integer is Fizz, Buzz or FizzBuzz.
+def fizz_or_buzz():
+    pass
+
+
+# Main function to hold main execution code.
+def main():
+    pass
+
+
+# Call main function to run upon execution of file.
+if __name__ == "__main__":
+    main()
